f62b8309108e6777a75defdfb4194ad00cd7ac78
SrieeVathsan <srieevathsanpr@gmail.com>
2025-07-02 13:33:54 +0530

    first commit - Netflix clone

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import FAQ from './components/FAQ';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Features />
+      <FAQ />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="/logo.png" alt="Netflix" className="logo" />
+      <div className="actions">
+        <select className="lang-select">
+          <option>English</option>
+          <option>தமிழ்</option>
+          <option>हिन्दी</option>
+        </select>
+        <button className="signin-btn">Sign In</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
